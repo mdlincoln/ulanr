f1ec710750112f190b966d9c2c14d0021c52b79c
mdlincoln <matthew.d.lincoln@gmail.com>
2015-05-03 17:38:26 -0400

    Break out tests into separate files
    One is for the top-level ulan_id method
    The other for the ulan_sparql handler

--- tests/testthat/test_ulanr.R
+++ tests/testthat/test_ulanr.R
@@ -1,36 +1,14 @@
 library(ulanr)
-context("SPARQL results")
-
-test_that("no matching results returns NA", {
-  expect_equal(ulan_id("asfjk"), c(NA))
-  expect_warning(ulan_id("asfjk"))
-  expect_equal(ulan_id(c("Rembrandt", NA)), c(500011051, NA))
-  expect_equal(ulan_id(""), c(NA))
-})
+context("ulan_id")
 
 test_that("NULL or NA names return NULL", {
   expect_error(ulan_id(NA))
   expect_error(ulan_id(NULL))
 })
 
-test_that("ulan_id handles a vector of names", {
-  expect_equal(ulan_id(c("Rembrandt", "Hendrik Hondius")), c(500011051, 500006788))
-})
-
-test_that("ulan_id returns correct name", {
-  expect_equal(ulan_id("Rembrandt"), 500011051)
-  expect_equal(ulan_id("Hendrik Hondius"), 500006788)
-})
-
 test_that("incorrect early_year and late_year pairings raise errors", {
   expect_error(ulan_id("Rembrandt", early_year = c(1500, 1700), late_year = c(1500, 1800)))
   expect_error(ulan_id("Rembrandt", late_year = c(1500, 1800)))
   expect_error(ulan_id("Rembrandt", early_year = c("a", "b")))
-})
-
-test_that("ulan_id date restrictions work", {
-  expect_equal(ulan_id("Rembrandt", early_year = 1600, late_year = 1700), 500011051)
-  expect_equal(ulan_id("Rembrandt", early_year = 1700, late_year = 1800), 500019719)
-  expect_equal(ulan_id("Rembrandt", early_year = 1890, late_year = 2000), 500006691)
 })
 
