2dfe0b282002ad07c61e6d277916203be0ce61fd
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-03-08 16:53:57 -0500

    just have one ulan_match function
    Will always return a DF, possibly with multiple results. Can wrap other formatting functions as needed.

--- R/ulanr.R
+++ R/ulanr.R
@@ -9,7 +9,7 @@
 #' @param late_year Match only artists who were born before this year.
 #' @param inclusive Logical. Should life dates be filtered inclusive of the
 #'   [early_year, late_year] range?
-validate_input <- function(names, early_year, late_year, inclusive) {
+validate_input <- function(names, early_year, late_year, inclusive, method, max_results) {
   # Check names validity
   if(class(names) != "character")
     stop("names should be a character vector")
@@ -38,9 +38,9 @@ unrecognized_method <- function() {
   stop("Method ", method, " is not recognized. Try ?ulan_id for help.")
 }
 
-#' Name to ULAN ID
+#' Match names to the Getty ULAN
 #'
-#' Queries the Getty ULAN to find the best matching ID for a given string. You
+#' Queries the Getty ULAN to find matching entries for a given string. You
 #' may filter the results by specifying an early or late date.
 #'
 #' @param names A character vector of names to match to a canonical ULAN id.
@@ -60,17 +60,30 @@ unrecognized_method <- function() {
 #'   FALSE will include artists whose life dates intersect with [early_year,
 #'   late_year]
 #' @param method This value determines which method will be used to match the
-#'   name to a canonical ULAN id.
-#' @param progress_bar Display or hide a progress bar. By default, will only
-#'   display a progress bar if processing more than 50 names. Passing TRUE will
-#'   force a progress bar to appear; passing FALSE will always hide it.
+#'   name to a canonical ULAN id. \code{sparql} will query the Getty's live
+#'   endpoint, relying on its Lucene index for finding close matches, while
+#'   \code{local} instead uses string distance measures based on a local table
+#'   of ULAN entries.
+#' @param max_results. The maximum number of results to return. Defaults to 5.
+#'   Depending on the query, the actual number of results returned may be lower.
+#'   When \code{method = "sparql"} is used, values over 50 will be ignored.
 #'
-#' @return A canonical ULAN id for an artist as a 9-digit integer. If no match
-#'   is found, then NULL will be returned isntead.
+#' @return A data frame with 7 columns and no more than \code{max_results} rows:
+#' \describe{
+#' \item{\code{name}}{character. Original input vector}
+#' \item{\code{id}}{integer. ULAN id}
+#' \item{\code{pref_name}}{character. ULAN preferred name}
+#' \item{\code{birth_year}}{integer. Artist birth year, if assigned.}
+#' \item{\code{death_year}}{integer. Artist death year, if assigned}
+#' \item{\code{gender}}{character. Artist gender, if assigned.}
+#' \item{\code{nationality}}{character. Artist nationality, if assigned.}
+#' \item{\code{score}}{numeric. The score of the result. When \code(method =
+#' "sparql"), this is the Lucene index score (a higher score for a closer
+#' match). When \code(method = "local"), it will instead be a scaled string
+#' distance score.}
+#' }
 #'
 #' @note \code{method = "sparql"} requires an internet connection.
-#'
-#' @seealso \link{ulan_data}
 #'
 #' @export
 #' @examples
@@ -78,67 +91,10 @@ unrecognized_method <- function() {
 #'                  late_year = 1700, method = "sparql")}
 #' \dontrun{ulan_id(c("Rembrandt", "Rothko"), early_year = c(1600, 1900),
 #'                  late_year = c(1700, 2000), method = "sparql")}
-ulan_id <- function(names, early_year = -9999, late_year = 2090, inclusive = TRUE, method = c("sparql", "stringdist"), progress_bar = "default") {
+ulan_match <- function(names, early_year = -9999, late_year = 2090, inclusive = TRUE, method = c("sparql", "local"), max_results = 5) {
 
   # Check names, early_year, and late_year for valid class, length, and value
-  validate_input(names, early_year, late_year, inclusive)
-
-  # Replace any NA values in early_year and late_year with default time range
-  if(any(is.na(early_year))) {
-    warning("NAs in early_year have been coerced to -9999")
-    early_year[is.na(early_year)] <- -9999
-  }
-
-  if(any(is.na(late_year))) {
-    warning("NAs in late_year have been coerced to 2090")
-    late_year[is.na(late_year)] <- 2090
-  }
-
-  # Dispatch name to query handler based on selected method
-  if(method == "sparql") {
-    ulan_sparql_id(names, early_year, late_year, inclusive, progress_bar)
-  } else if(method == "stringdist") {
-    # Check that ulanrdata is installed
-    check_ulanrdata_package()
-    ulan_stringdist_id(names, early_year, late_year, inclusive, progress_bar)
-  } else {
-    unrecognized_method()
-  }
-}
-
-#' Name to ULAN data
-#'
-#' Queries the Getty ULAN to find the best matching ID for a given string,
-#' returning the ID as well as various artist attributes. You may filter the
-#' results by specifying an early or late date.
-#'
-#' @inheritParams ulan_id
-#'
-#' @return A data frame with 6 columns.
-#' \describe{
-#' \item{\code{name}}{Original input vector}
-#' \item{\code{id}}{ULAN id}
-#' \item{\code{pref_name}}{ULAN preferred name}
-#' \item{\code{startdate}}{Artist birth date}
-#' \item{\code{enddate}}{Artist death date}
-#' \item{\code{gender}}{Artist gender}
-#' \item{\code{nationality}}{Artist nationality}
-#' }
-#'
-#' @note \code{method = "sparql"} requires an internet connection.
-#'
-#' @seealso \link{ulan_id}
-#'
-#' @export
-#' @examples
-#' \dontrun{ulan_data("Rembrandt", early_year = 1600,
-#'                  late_year = 1700, method = "sparql")}
-#' \dontrun{ulan_data(c("Rembrandt", "Rothko"), early_year = c(1600, 1900),
-#'                  late_year = c(1700, 2000), method = "sparql")}
-ulan_data <- function(names, early_year = -9999, late_year = 2090, inclusive = TRUE, method = c("sparql"), progress_bar = "default") {
-
-  # Check names, early_year, and late_year for valid class, length, and value
-  validate_input(names, early_year, late_year, inclusive)
+  validate_input(names, early_year, late_year, inclusive, method, max_results)
 
   # Replace any NA values in early_year and late_year with default time range
   if(any(is.na(early_year))) {
