b3e246dd6c6b26181359f657b61021cfc88905ef
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-03-08 22:02:44 -0500

    fix many outdated variable names here

--- R/ulan_stringdist.R
+++ R/ulan_stringdist.R
@@ -1,96 +1,75 @@
 # Run the table lookup using stringdist function, returning a data frame of all matches
 ulan_stringdist_lookup <- function(name, early_year, late_year, inclusive, stringdist_ops = NULL) {
   # Return NA for missing or empty values of name
-  if(is.null(name))
-    return(NA)
-  if(is.na(name))
-    return(NA)
-  if(name == "")
+  if(any(is.null(name), is.na(name), name == ""))
     return(NA)
 
   # Strip punctuation from name string
-  name <- tolower(gsub("[[:punct:]]", "", name))
+  name <- trimws(tolower(gsub("[[:punct:]]", "", name)))
 
   # Should life dates be a subset of early_year and late_year, or merely
   # intersecting with early_year and late_year?
   if(inclusive) {
-    score_table <- dplyr::filter(ulanrdata::query_table, birth <= late_year & death >= early_year)
+    score_table <- dplyr::filter(ulanrdata::query_table, birth_year <= late_year & death_year >= early_year)
   } else {
-    score_table <- dplyr::filter(ulanrdata::query_table, birth >= early_year & death <= late_year)
+    score_table <- dplyr::filter(ulanrdata::query_table, birth_year >= early_year & death_year <= late_year)
   }
 
-  # Calculate string distance scores
-  score_table <- dplyr::mutate(score_table, score = stringdist::stringdist(alt_name, name))
+  # Look for exact matches - if we find them, then it's not necessary to do any
+  # costly string distance calculations
+  match_tries <- dplyr::filter(score_table, alt_name == name)
 
-  # Sort by string distance score
-  score_table <- dplyr::arrange(score_table, score)
+  if(nrow(match_tries) > 0) {
+    match_tries$score <- 10
+    return(match_tries)
+  }
+
+  # Calculate string distance scores, rescaling scores 0-10
+  score_table <- dplyr::mutate(score_table, score = stringdist::stringdist(alt_name, name))
+  score_table <- dplyr::mutate(score_table, score = 10/(score + 0.1))
+
+  # Sort by inverse score
+  score_table <- dplyr::distinct(dplyr::arrange(score_table, desc(score)), id)
 
   return(score_table)
 }
 
-# Returns a single ID
-ulan_stringdist_id_handler <- function(name, early_year, late_year, inclusive, stringdist_ops = NULL) {
-
-  score_table <- ulan_stringdist_lookup(name, early_year, late_year, inclusive, stringdist_ops = NULL)
-
-  if(score_table$score[1] > 10) {
-    warning("No matches found for the following name: ", name)
-    return(NA)
-  } else {
-    score_table$id[1]
-  }
-}
-
 # Returns one-row data frame with artist attributes
-ulan_stringdist_data_handler <- function(name, early_year, late_year, inclusive, stringdist_ops = NULL) {
+ulan_stringdist_match_handler <- function(name, early_year, late_year, inclusive, max_results, stringdist_ops = NULL) {
 
-  score_table <- ulan_stringdist_lookup(name, early_year, late_year, inclusive, stringdist_ops = NULL)
+  score_table <- ulan_stringdist_lookup(name, early_year, late_year, inclusive, stringdist_ops)
 
-  if(score_table$score[1] > 10) {
+  if(is.null(nrow(score_table)) | nrow(score_table) == 0) {
     warning("No matches found for the following name: ", name)
     return(dplyr::data_frame(
+      name = name,
       id = NA,
       pref_name = NA,
-      birth = NA,
-      death = NA,
+      birth_year = NA,
+      death_year = NA,
       gender = NA,
       nationality = NA,
-      name = name
+      score = NA
     ))
   } else {
-    results <- dplyr::select(score_table, -score)[1,]
-    results$name <- name
-    return(results)
+    score_table$name <- name
+    score_table <- dplyr::slice(score_table, 1:max_results)
+    dplyr::select(score_table, name, id, pref_name, birth_year, death_year, gender, nationality, score)
   }
 }
 
-ulan_stringdist_id <- function(names, early_year, late_year, inclusive, progress_bar) {
+ulan_stringdist_match <- function(names, early_year, late_year, inclusive, max_results) {
   # For long queries or if explicitly set, create and increment txtProgressBar
-  if((progress_bar == "default" & length(names) >= 50) | progress_bar == TRUE) {
+  if(use_pb(names)) {
     pb <- txtProgressBar(min = 0, max = length(names), style = 3)
-    ids <- mapply(function(a, b, c, d) {
+    ids <- mapply(function(a, b, c) {
       setTxtProgressBar(pb, (getTxtProgressBar(pb) + 1))
-      ulan_stringdist_id_handler(a, b, c, d)},
-      names, early_year, late_year, inclusive, SIMPLIFY = TRUE, USE.NAMES = FALSE)
-    close(pb)
-    return(ids)
-  } else {
-    mapply(function(a, b, c, d) ulan_stringdist_id_handler(a, b, c, d), names, early_year, late_year, inclusive, SIMPLIFY = TRUE, USE.NAMES = FALSE)
-  }
-}
-
-ulan_stringdist_data <- function(names, early_year, late_year, inclusive, progress_bar) {
-  # For long queries or if explicitly set, create and increment txtProgressBar
-  if((progress_bar == "default" & length(names) >= 50) | progress_bar == TRUE) {
-    pb <- txtProgressBar(min = 0, max = length(names), style = 3)
-    ids <- mapply(function(a, b, c, d) {
-      setTxtProgressBar(pb, (getTxtProgressBar(pb) + 1))
-      ulan_stringdist_data_handler(a, b, c, d)},
-      names, early_year, late_year, inclusive, SIMPLIFY = FALSE, USE.NAMES = FALSE)
+      ulan_stringdist_match_handler(a, b, c, inclusive, max_results)},
+      names, early_year, late_year, SIMPLIFY = FALSE, USE.NAMES = FALSE)
     close(pb)
     dplyr::bind_rows(ids)
   } else {
-    ids <- mapply(function(a, b, c, d) ulan_stringdist_data_handler(a, b, c, d), names, early_year, late_year, inclusive, SIMPLIFY = FALSE, USE.NAMES = FALSE)
+    ids <- mapply(function(a, b, c) ulan_stringdist_match_handler(a, b, c, inclusive, max_results), names, early_year, late_year, SIMPLIFY = FALSE, USE.NAMES = FALSE)
     dplyr::bind_rows(ids)
   }
 }
