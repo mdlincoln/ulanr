4306e7b8a4432e36b529f77f8ac3d4326bea53fa
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-03-08 20:17:29 -0500

    update stringdist tests to new command

--- tests/testthat/test_ulan_stringdist.R
+++ tests/testthat/test_ulan_stringdist.R
@@ -1,28 +1,32 @@
 library(ulanr)
-context("stringdist results")
+library(dplyr, warn.conflicts = FALSE)
+context("SPARQL data results")
+
+na_df <- function(name) {
+  data_frame(
+    name = name,
+    id = c(NA),
+    pref_name = c(NA),
+    birth_year = c(NA),
+    death_year = c(NA),
+    gender = c(NA),
+    nationality = c(NA),
+    score = c(NA))
+}
+
+val_df <- data_frame(
+  name = character(1),
+  id = integer(1),
+  pref_name = character(1),
+  birth_year = integer(1),
+  death_year = integer(1),
+  gender = character(1),
+  nationality = character(1),
+  score = double(1))
 
 test_that("no matching results returns NA", {
-  expect_equal(ulan_id("asfjk", method = "stringdist"), c(NA))
-  expect_warning(ulan_id("asfjk"), method = "stringdist")
-  expect_equal(ulan_id(c("Rembrandt", NA), method = "stringdist"), c(500011051, NA))
-  expect_equal(ulan_id("", method = "stringdist"), c(NA))
-})
-
-test_that("ulan_id handles a vector of names", {
-  expect_equal(ulan_id(c("Rembrandt", "Hendrick Hondius (I)"), method = "stringdist"), c(500011051, 500006788))
-})
-
-test_that("multiple names can be queried using one year range", {
-  expect_equal(ulan_id(c("Rembrandt", "Hendrick Hondius (I)"), early_year = 1500, late_year = 1700, method = "stringdist"), c(500011051, 500006788))
-})
-
-test_that("ulan_id returns correct name", {
-  expect_equal(ulan_id("Rembrandt", method = "stringdist"), 500011051)
-  expect_equal(ulan_id("Hendrik Hondius (I)", method = "stringdist"), 500006788)
-})
-
-test_that("ulan_id date restrictions work", {
-  expect_equal(ulan_id("Rembrandt", early_year = 1600, late_year = 1700, method = "stringdist"), 500011051)
-  expect_equal(ulan_id("Rembrandt", early_year = 1700, late_year = 1800, method = "stringdist"), 500019719)
-  expect_equal(ulan_id("Rembrandt", early_year = 1890, late_year = 2000, method = "stringdist"), 500006691)
+  expect_equivalent(lapply(ulan_match("asfjk", method = "local"), is.na), list(is.na(na_df("asfjk"))))
+  expect_warning(ulan_match("asfjk", method = "local"))
+  expect_equivalent(lapply(ulan_match(c("Rembrandt van Rijn", NA), method = "local", max_results = 1), is.na), lapply(list(val_df, na_df(NA)), is.na))
+  expect_equivalent(lapply(ulan_match("", method = "local"), is.na), lapply(list(na_df("")), is.na))
 })
