dc3c1202eb1bf09a7f0a1778e15e77fe1958fd04
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-05-15 11:07:06 -0400

    Construct query table with multiple SPARQL calls
    The SPARQL call for the complete table is way too large, and requires downloading a lot of duplicate information. As an alternative, we can download just the alntames, and a table of the attributes, and do the joining within R. This is saved as an .rds file in the ulanrdb package installation.

--- R/build_ulanrdb.R
+++ R/build_ulanrdb.R
@@ -7,48 +7,87 @@ build_ulanrdb <- function() {
   # First check that ulanrdb is installed
   check_ulanrdb_package()
 
-  dbpath <- ulanrdb_path()
-
-  query <- "SELECT DISTINCT ?id ?pref_name ?alt_name ?birth_year ?death_year ?nationality ?gender
-  WHERE {
-  ?artist skos:inScheme ulan: ;
-  dc:identifier ?id ;
-  rdf:type gvp:PersonConcept ;
-  xl:prefLabel [xl:literalForm ?pref_name] .
-
-  OPTIONAL {
-  { ?artist xl:altLabel [xl:literalForm ?alt_name] . } UNION
-  { ?artist xl:prefLabel [xl:literalForm ?alt_name] . }
+  if(file.exists(ulanrdb_path())) {
+    input <- menu(c("Yes", "No"), title = paste0("Local database already exists. Overwrite?"))
+    if(input == 2) {
+      message("Not overwriting ", ulanrdb_path())
+      return()
+    }
   }
 
-  OPTIONAL {
-  ?artist foaf:focus [gvp:biographyPreferred ?bio] .
-
-  OPTIONAL {
-  ?bio gvp:estStart ?birth_year ;
-  gvp:estEnd ?death_year .
-  }
-
-  OPTIONAL {
-  ?bio schema:gender [gvp:prefLabelGVP [gvp:term ?gender]] .
-  FILTER(langMatches(lang(?gender), \"EN\"))
-  }
-  }
-
-  OPTIONAL {
-  ?artist foaf:focus [gvp:nationalityPreferred [xl:prefLabel [gvp:term ?nationality]]] .
-  FILTER(langMatches(lang(?nationality), \"EN\"))
-  }
-}"
-
-  input <- menu(c("Yes", "No"), title = paste0("Downloading table to ", dbpath, ". The file size is usually ~16MB. Proceed? (y/n)"))
+  input <- menu(c("Yes", "No"), title = paste0("Downloading tables to ", ulanrdb_path(), ". The total download size is usually ~26.5MB. Proceed?"))
   if(input == 1) {
-    httr::GET(construct_sparql_url(query), write_disk(dbpath, overwrite = TRUE), progress())
+    build_tables(ulanrdb_path())
   } else {
     stop("A local ULAN database must be built in order to proceed.")
   }
+}
 
-    # attr_tbl <- readr::read_csv(dbpath, col_types = c("icciicc"))
+db_exists <- function(tbl_path) {
+  file.exists()
+}
+
+build_tables <- function(tbl_path) {
+  message("Downloading alternate names table (~18MB)")
+  id_response <- httr::GET(construct_sparql_url(id_altname_query()), httr::progress())
+  message("Downloading attributes table (~8.5MB)")
+  attr_response <- httr::GET(construct_sparql_url(id_attributes_query()), httr::progress())
+
+  # Read tables into dataframes
+  message("Parsing downloads...")
+  id_tbl <- readr::read_csv(httr::content(id_response, as = "text"), col_types = "ic")
+  attr_tbl <- readr::read_csv(httr::content(attr_response, as = "text"), col_types = "iciicc")
+  query_table <- dplyr::distinct(
+    dplyr::mutate(dplyr::left_join(id_tbl, attr_tbl, by = "id"),
+                  alt_name = tolower(gsub("[[:punct:]]", "", alt_name))))
+
+  message("Saving final table to ", tbl_path)
+  saveRDS(query_table, file = tbl_path, overwrite = TRUE)
+}
+
+# Retrieve Table combining all IDs with both pref and alt names unified in one
+# column, which can be used as a search mechanism
+id_altname_query <- function() {
+  "SELECT DISTINCT ?id ?alt_name
+  WHERE {
+    ?artist skos:inScheme ulan: ;
+            rdf:type gvp:PersonConcept ;
+            dc:identifier ?id .
+
+    { ?artist xl:altLabel [xl:literalForm ?alt_name] . } UNION
+    { ?artist xl:prefLabel [xl:literalForm ?alt_name] . }
+  }"
+}
+
+# Creates a shorter, but wider table with preferred names and biographical
+# details, when available
+id_attributes_query <- function() {
+  "SELECT DISTINCT ?id ?pref_name ?birth_year ?death_year ?nationality ?gender
+  WHERE {
+    ?artist skos:inScheme ulan: ;
+            dc:identifier ?id ;
+            rdf:type gvp:PersonConcept ;
+            dc:identifier ?id ;
+            xl:prefLabel [xl:literalForm ?pref_name] .
+
+    OPTIONAL {
+      ?artist foaf:focus [gvp:biographyPreferred ?bio] .
+
+      OPTIONAL {
+        ?bio gvp:estStart ?birth_year ;
+             gvp:estEnd ?death_year .
+      }
+
+      OPTIONAL {
+        ?bio schema:gender [gvp:prefLabelGVP [gvp:term ?gender]] .
+      }
+    }
+
+    OPTIONAL {
+      ?artist foaf:focus [gvp:nationalityPreferred [xl:prefLabel [gvp:term ?nationality]]] .
+      FILTER(langMatches(lang(?nationality), \"EN\"))
+    }
+  }"
 }
 
 construct_sparql_url <- function(query) {
@@ -57,9 +96,8 @@ construct_sparql_url <- function(query) {
   paste0(endpoint, ".csv?query=", escaped_query)
 }
 
-
 ulanrdb_path <- function() {
-  paste0(system.file("db", package = "ulanrdb", mustWork = TRUE), "/ulan_table.csv")
+  paste0(system.file("db", package = "ulanrdb", mustWork = TRUE), "/ulan_table.rds")
 }
 
 check_ulanrdb_package <- function() {
